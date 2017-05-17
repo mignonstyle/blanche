09ed80af30f7aca4264e8574cc98c80250af84d6
mignonstyle <mignonxstyle@gmail.com>
2017-05-17 15:00:58 +0900

    Change display of archive title.

--- inc/extras.php
+++ inc/extras.php
@@ -23,6 +23,49 @@ function blanche_pingback_header() {
 add_action( 'wp_head', 'blanche_pingback_header', -9999 );
 
 /**
+ * Change display of archive title.
+ */
+function blanche_get_the_archive_title( $title ) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>';
+
+	} elseif ( is_year() ) {
+		$title = get_the_date( _x( 'Y', 'yearly archives date format' ) );
+
+	} elseif ( is_month() ) {
+		$title = get_the_date( _x( 'F Y', 'monthly archives date format' ) );
+
+	} elseif ( is_day() ) {
+		$title = get_the_date( _x( 'F j, Y', 'daily archives date format' ) );
+
+	} elseif ( is_post_type_archive() ) {
+		$title = post_type_archive_title( '', false );
+
+	}
+
+	return $title;
+}
+add_action( 'get_the_archive_title', 'blanche_get_the_archive_title' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
  * Specify the number of characters in the excerpt.
  */
 if ( ! function_exists( 'blanche_excerpt_length' ) ) :
